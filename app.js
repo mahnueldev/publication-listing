bf74bb86161002fef7de393a1930e46ea1a238f6
mahnueldev <emmanuelgbd3@gmail.com>
2023-04-20 14:19:40 +0100

    code addded

--- app.js
+++ app.js
@@ -43,6 +43,14 @@ function UI() {
       document.querySelector('.alert').remove();
     }, 3000);
   };
+
+  // Delete book
+  UI.prototype.deleteBook = function(target){
+    if(target.className === 'delete') {
+      target.parentElement.parentElement.remove();
+    }
+     
+  }
   // Clear Fields
   UI.prototype.clearFields = function () {
     document.getElementById('title').value = '';
@@ -74,6 +82,20 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     //   Clear Fields
     ui.clearFields();
   }
+ 
+});
+
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function(e){
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Delete book
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
 
   e.preventDefault();
 });
